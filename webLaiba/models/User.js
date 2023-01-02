2843ff5ecf87d2eceeb4772f6f1222f1ca015999
Laiba Sajid <laibasajidsaleemi24@gmail.com>
2023-01-02 23:14:45 +0500

    Delete webLaiba directory

--- webLaiba/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  roles: [String],
-});
-const User = mongoose.models.User || mongoose.model("User", userSchema);
-module.exports = User;
